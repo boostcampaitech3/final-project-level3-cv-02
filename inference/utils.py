37efbd944c0b43bfdcf4ec8b9208b74d72921156
Juyoung Kim <ingressor531@gmail.com>
2022-06-01 16:12:03 +0900

    fix: 코드 import 경로 오류 수정

--- inference/utils.py
+++ inference/utils.py
@@ -1,8 +1,7 @@
 import torch
 import warnings
-from runners.image_editing import *
+from image_editing import *
 from main import dict2namespace
-from functions.process_data import *
 import os
 import yaml
 import sys
